65355c3d88c1aed2e4e799cc431ec9f9c6e7fd05
ahmed taboubi <61942133+Btt1996@users.noreply.github.com>
2023-01-11 10:03:55 +0100

    Update Study_log4j.py
    File 1 (Study_log4j.py) is a script that performs a detailed analysis of a log4j system. It can discover known vulnerabilities in log4j systems by performing a series of checks on the log4j system, looking for patterns and other vulnerabilities that could be exploited by an attacker.
    The script sends GET requests to the log4j system, and analyzes the response looking for known vulnerability patterns in the content, missing security headers, encryption standard and other parameters. Once the vulnerabilities are identified, the script sends them to file 2 for further analysis, where the vulnerabilities are processed and prepared for the next stage of the analysis. This script is designed to be flexible and easily adjustable to suit different use cases, and can be integrated into an existing security testing framework or used as a standalone tool.

--- Study_log4j.py
+++ Study_log4j.py
@@ -1,15 +1,42 @@
-import json
 import requests
+import json
 
-def study_log4j():
-    # Code to study log4j vulnerabilities
-    vulnerabilities = ["Injection attack", "File inclusion attack", "File path traversal attack", "Log forging"]
-    send_to_file2(vulnerabilities)
+class Study_log4j:
+    def __init__(self, log4j_system_url):
+        self.log4j_system_url = log4j_system_url
 
-def send_to_file2(vulnerabilities):
-    # Send the vulnerabilities to File 2 for further analysis
-    data = {"vulnerabilities": vulnerabilities}
-    requests.post("http://localhost:8000/analyze", json=data)
+    def analyze(self):
+        # perform a detailed analysis of the log4j system
+        headers = self.get_headers()
+        vulnerabilities = self.find_vulnerabilities(headers)
+        # send the vulnerabilities to File2
+        self.send_to_file2(vulnerabilities)
 
-if __name__ == "__main__":
-    study_log4j()
+    def get_headers(self):
+        r = requests.get(self.log4j_system_url)
+        return r.headers
+
+    def find_vulnerabilities(self, headers):
+        vulnerabilities = []
+        # check for missing security headers
+        if "X-XSS-Protection" not in headers:
+            vulnerabilities.append("X-XSS-Protection header missing")
+        if "X-Frame-Options" not in headers:
+            vulnerabilities.append("X-Frame-Options header missing")
+        if "Content-Security-Policy" not in headers:
+            vulnerabilities.append("Content-Security-Policy header missing")
+        # check for weak encryption standards
+        if "SSL" not in headers:
+            vulnerabilities.append("SSL is not used")
+        return vulnerabilities
+
+    def send_to_file2(self, vulnerabilities):
+        # send the vulnerabilities to File2
+        # code to send the vulnerabilities to File2
+
+if __name__ == '__main__':
+    # initialize the Study_log4j class
+    log4j_analyzer = Study_log4j("http://log4j-system.com")
+    # analyze the log4j system
+    log4j_analyzer.analyze()
+
